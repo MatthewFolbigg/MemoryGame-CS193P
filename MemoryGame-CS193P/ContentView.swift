cf643c0fc384e6e14bc66fa9ae85f22f0df60278
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-05-18 18:03:53 +0100

    Began following the Stanford CS193P 2021 lecture series. This commit is lecture work from week 1.
    Lecture 1 and Lecture 2
    
    - Added basic card ui for both face up and face down
    - added gesture to flip the card (currently an @state in the UI code for testing purposes)
    - added ability to add and remove cards from an array of random emoji (For testing)

--- MemoryGame-CS193P/ContentView.swift
+++ MemoryGame-CS193P/ContentView.swift
@@ -8,14 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["💻", "💾", "💿", "📽", "🎞", "🕹", "📼", "📱", "🎚", "📺", "📡", "🔫", "🔌", "💡", "📞", "🎙", "☎️", "🔦", "💣", "🧲", "🔭", "🔬", "🪓", "🔪", "🔩"]
+    @State var cardCount = 5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                    ForEach(emojis[0..<cardCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+                .padding([.top, .leading, .trailing])
+            }.padding(0)
+            .foregroundColor(.green)
+            Spacer()
+            HStack {
+                removeCardButton
+                Spacer()
+                addCardButton
+            }
+            .font(.title)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal, 1)
+    }
+    
+    var addCardButton: some View {
+        Button {
+            if cardCount < emojis.count { cardCount += 1 }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+    
+    var removeCardButton: some View {
+        Button {
+            if cardCount > 1 { cardCount -= 1 }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
     }
 }
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack() {
+            let cardShape = RoundedRectangle(cornerRadius: 15, style: .continuous)
+            if isFaceUp {
+                cardShape.fill().foregroundColor(.white)
+                cardShape.strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                cardShape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+            .previewDevice("iPhone 12 mini")
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 12 mini")
     }
 }
