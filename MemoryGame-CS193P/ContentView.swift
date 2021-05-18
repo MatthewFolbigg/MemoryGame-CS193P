9c889d9f0311fb6972703a1132d6dd5ad74ccad9
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-05-18 15:22:46 +0100

    Initial Commit

---
+++ MemoryGame-CS193P/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoryGame-CS193P
+//
+//  Created by Matthew Folbigg on 18/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoryGame-CS193P/MemoryGame_CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame_CS193PApp.swift
+//  MemoryGame-CS193P
+//
+//  Created by Matthew Folbigg on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGame_CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
